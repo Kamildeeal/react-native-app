4a90bb907af2b1ca8d1a4f4989fefeb97c07cbc1
Kamildeeal <kamil.kamil.1995@wp.pl>
2024-07-26 21:06:04 +0200

    initial commit

---
+++ app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import tw from '../twrnc';
+
+const App = () => {
+  return (
+    <View>
+      <Text style={tw`text-5xl`}>KamAppShop Work</Text>
+      <Text>elo</Text>
+    </View>
+  );
+};
+
+export default App;
---
+++ twrnc.js
@@ -0,0 +1,6 @@
+import {create} from 'twrnc';
+
+// Możesz zmodyfikować ścieżkę do pliku konfiguracyjnego Tailwind CSS, jeśli jest inna.
+const tw = create(require(`./tailwind.config.js`));
+
+export default tw;
